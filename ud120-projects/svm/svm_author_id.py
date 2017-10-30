0eae75a549f30d2d9df5133cd136072b53222743
Mengjie SHI <mengjie.shi@airweb.fr>
2017-10-30 14:10:13 +0100

    svm mini project

--- ud120-projects/svm/svm_author_id.py
+++ ud120-projects/svm/svm_author_id.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+
+from sklearn.svm import SVC
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +22,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# 减小数据集
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
+clf = SVC(C=10000.0,kernel='rbf')
 
-#########################################################
-### your code goes here ###
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
-#########################################################
+print clf.score(features_test, labels_test)
 
+predictions = clf.predict(features_test)
 
+print "Predctions:"
+
+print len(predictions[predictions==1])
