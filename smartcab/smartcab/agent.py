e9ec6243c3910a7f94bf1bf1e793a122620ca704
Mengjie SHI <mengjie.shi@airweb.fr>
2017-12-12 18:19:55 +0100

    Smartcab:default learning

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -8,7 +8,7 @@ class LearningAgent(Agent):
     """ An agent that learns to drive in the Smartcab world.
         This is the object you will be modifying. """
 
-    def __init__(self, env, learning=False, epsilon=1.0, alpha=0.5):
+    def __init__(self, env, learning=False, epsilon=1.0, alpha=0.4):
         super(LearningAgent, self).__init__(env)     # Set the agent in the evironment
         self.planner = RoutePlanner(self.env, self)  # Create a route planner
         self.valid_actions = self.env.valid_actions  # The set of valid actions
@@ -23,7 +23,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
-
+        self.t = 0
 
     def reset(self, destination=None, testing=False):
         """ The reset function is called at the beginning of each trial.
@@ -39,6 +39,9 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
+        # y = (100-1.01^x)/100
+        self.epsilon *= 0.99
+        self.t +=1
 
         return None
 
@@ -51,7 +54,7 @@ class LearningAgent(Agent):
         waypoint = self.planner.next_waypoint() # The next waypoint
         # ex. waypoint = ['forward', 'left', 'right']
         inputs = self.env.sense(self)           # Visual input - intersection light and traffic
-        # ex. inputs = {'light': light, 'oncoming': oncoming, 'left': left, 'right': right}
+        # ex. inputs = {'light': 'red', 'oncoming': None, 'left': 'right', 'right': 'forward'}
         deadline = self.env.get_deadline(self)  # Remaining deadline
         # ex. deadline =
 
@@ -59,7 +62,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set 'state' as a tuple of relevant data for the agent
-        state = None
+        state = (waypoint, inputs['light'], inputs['oncoming'], inputs['left'], inputs['right'])
 
         return state
 
@@ -75,6 +78,10 @@ class LearningAgent(Agent):
 
         maxQ = None
 
+        for key, value in self.Q[state].items():
+            if maxQ == None or value > maxQ[1]:
+                maxQ = (key, value)
+
         return maxQ
 
 
@@ -87,6 +94,14 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
+
+        if state not in self.Q.keys():
+            initial_Q_value = dict()
+            initial_Q_value['right'] = 0
+            initial_Q_value['left'] = 0
+            initial_Q_value['forward'] = 0
+            initial_Q_value[None] = 0
+            self.Q[state] = initial_Q_value
 
         return
 
@@ -105,10 +120,16 @@ class LearningAgent(Agent):
         ###########
         # When not learning, choose a random action
         action_list = [None, 'left', 'right', 'forward']
-        random_action_index = random.randint(0,3)
-        action = action_list[random_action_index]
         # When learning, choose a random action with 'epsilon' probability
         #   Otherwise, choose an action with the highest Q-value for the current state
+
+        probability = random.random()
+        if probability < self.epsilon:
+            random_action_index = random.randint(0,3)
+            action = action_list[random_action_index]
+        else:
+            maxQ = self.get_maxQ(state)
+            action = maxQ[0]
 
         return action
 
@@ -123,6 +144,14 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
+
+        # use following function to update q value
+        # Q(S,A) - (1-alpha)*Q(S,A) + alpha*[R + gamma*maxQ(S',a)]
+        # gamma = 1
+
+        Q_next = self.get_maxQ(state) # tuple (action , qvalue)
+        Q_actual = self.Q[state][action] # qvalue
+        self.Q[state][action] = (1-self.alpha)*Q_actual + self.alpha*(reward + Q_next[1])
 
         return
 
@@ -151,7 +180,7 @@ def run():
     #   verbose     - set to True to display additional output from the simulation
     #   num_dummies - discrete number of dummy agents in the environment, default is 100
     #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
-    env = Environment(verbose=True)
+    env = Environment(verbose=False)
 
     ##############
     # Create the driving agent
@@ -174,14 +203,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True)
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, optimized=True)
 
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(tolerance=0.01, n_test=20)
 
 
 if __name__ == '__main__':
